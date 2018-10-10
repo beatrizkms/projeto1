b4e575fca9c403131263a0164088d45974a7194f
beatrizkms <beatrizkms@gmail.com>
2018-10-10 15:30:52 -0300

    Update index.php
    testando conexao com o mysql

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php 
-echo 'Index<br/>';
+//phpinfo(); exit;
 
+echo 'Index<br/>';
 echo 'Alteracao para o branch1...';
 
+// conexao com o mysql
+$bd = mysqli_connect('localhost','root','','projeto1');
+//debug($bd);
 
+$rs = $bd->query('select * from usuario');
+debug($rs);
+
+if ($rs) {
+	foreach($rs as $row) {
+		debug($row);
+	}
+}
+
+
+
+function debug($var,$vardump=false) {
+	echo '<pre>';
+	if ($vardump) {
+		var_dump($var);
+	} else {
+		print_r($var);
+	}
+	echo '</pre>';
+}
 ?>
